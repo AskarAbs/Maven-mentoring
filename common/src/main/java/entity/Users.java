5bdcccaa655c56026188960cb7d926d357012d01
Askar <askarabsattarov1310@gmail.com>
2024-02-11 23:27:30 +0300

    Добавил password

--- common/src/main/java/entity/Users.java
+++ common/src/main/java/entity/Users.java
@@ -23,6 +23,7 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    private String password;
     private String email;
     @Enumerated(EnumType.STRING)
     private Role role;
